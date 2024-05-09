894faf04a5fa0b4b0fa6f5ffce143ba88d308b68
Rafaelo <gjergjirafo@gmail.com>
2024-05-09 09:17:11 -0400

    update files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         'brand-blue': '#0056b3',
         'light-blue': '#3b82f6',
         'brand-grey': '#d1d5db',
-      }
+      },
+      fontFamily: {
+        'sans': ['Inter', 'system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
     },
   },
   plugins: [],
