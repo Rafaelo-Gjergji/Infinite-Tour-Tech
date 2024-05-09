b4ee527bd8b998b2cbbee106eb3cd44fa5b4734a
Rafaelo <gjergjirafo@gmail.com>
2024-05-08 22:33:50 -0400

    added login, register page

---
+++ frontend/src/components/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>Please enter your credentials to log in.</p>
+      {/* Future login form goes here */}
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ frontend/src/components/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function RegisterPage() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <p>Create an account to use our services.</p>
+      {/* Future registration form goes here */}
+    </div>
+  );
+}
+
+export default RegisterPage;
