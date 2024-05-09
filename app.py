7259903f07df47b33d613738dc9052b602742673
Rafaelo <gjergjirafo@gmail.com>
2024-05-08 22:19:05 -0400

    created vite frontend

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -8,3 +9,7 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///InfiniteTourTech.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
