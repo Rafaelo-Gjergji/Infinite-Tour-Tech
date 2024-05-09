a4684ac041c104e1e8ba9ba7c0b2abcb4fec63cf
Rafaelo <gjergjirafo@gmail.com>
2024-05-09 06:11:29 -0400

    update files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,14 @@
-import './global.css';
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { Provider } from "react-redux";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import store from "./store/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
+    <Provider store={store}>
       <App />
-    </AuthProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
